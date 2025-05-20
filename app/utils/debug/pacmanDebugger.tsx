beca5b251c12b9bacaffc3ba57424a68fdf2e0b7
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-20 09:35:57 -0300

    Create pacmanDebugger.tsx

---
+++ app/utils/debug/pacmanDebugger.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DIRECTIONS, PACMAN_STATES } from "~/consts/game";
+import { RootState } from "~/store";
+import { PacmanActions } from "~/store/pacmanSlice";
+
+export default function PacmanDebugger() {
+  const dispatch = useDispatch();
+  const pacman = useSelector((state: RootState) => state.pacman);
+
+  function handleStateChange(state: string) {
+    dispatch(PacmanActions.setState(state));
+  }
+  function handleDirectionChange(dir: string) {
+    dispatch(PacmanActions.setDirection(dir));
+  }
+
+  return (
+    <article className="absolute flex flex-col gap-4 p-4 rounded-lg top-10 left-10 bg-slate-800">
+      <h2 className="text-center text-yellow-400 uppercase">Pacman Debugger</h2>
+      <h3>States</h3>
+      <ul className="grid grid-cols-3 gap-2 text-xs text-center">
+        <li
+          onClick={() => handleStateChange(PACMAN_STATES.IDLE)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {PACMAN_STATES.IDLE}
+        </li>
+        <li
+          onClick={() => handleStateChange(PACMAN_STATES.DYING)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {PACMAN_STATES.DYING}
+        </li>
+        <li
+          onClick={() => handleStateChange(PACMAN_STATES.MOVING)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {PACMAN_STATES.MOVING}
+        </li>
+        <li
+          onClick={() => handleStateChange(PACMAN_STATES.EATING_POWER_PELLET)}
+          className="col-span-3 py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {PACMAN_STATES.EATING_POWER_PELLET}
+        </li>
+        <li
+          onClick={() => handleStateChange(PACMAN_STATES.EATING_GHOST)}
+          className="col-span-3 py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {PACMAN_STATES.EATING_GHOST}
+        </li>
+      </ul>
+      <h3>Directions</h3>
+      <ul className="grid grid-cols-2 gap-2 text-xs text-center">
+        <li
+          onClick={() => handleDirectionChange(DIRECTIONS.UP)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {DIRECTIONS.UP}
+        </li>
+        <li
+          onClick={() => handleDirectionChange(DIRECTIONS.LEFT)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {DIRECTIONS.LEFT}
+        </li>
+        <li
+          onClick={() => handleDirectionChange(DIRECTIONS.DOWN)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {DIRECTIONS.DOWN}
+        </li>
+        <li
+          onClick={() => handleDirectionChange(DIRECTIONS.RIGHT)}
+          className="py-1 bg-slate-600 ring-1 ring-slate-400 hover:bg-slate-300 hover:ring-slate-50 hover:text-black hover:cursor-pointer"
+        >
+          {DIRECTIONS.RIGHT}
+        </li>
+      </ul>
+      <h3>Current values</h3>
+      <ul className="grid grid-cols-2 gap-2 text-xs text-center">
+        <li>X: {pacman.x}</li>
+        <li>Sta: {pacman.state}</li>
+        <li>Y: {pacman.y}</li>
+        <li>dir: {pacman.direction}</li>
+      </ul>
+    </article>
+  );
+}
