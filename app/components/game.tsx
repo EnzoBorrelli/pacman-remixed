86a565d93a515730a6d574c7f2cc90b9fdfcc22b
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-20 09:37:45 -0300

    added pacman and ghosts

--- app/components/game.tsx
+++ app/components/game.tsx
@@ -1,12 +1,28 @@
 import { Stage } from "@pixi/react";
 import Level from "./level";
 import { CHAR_SPAWNS, DIRECTIONS, SIZES } from "~/consts/game";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "~/store";
 import Pacman from "./pacman";
+import { useEffect } from "react";
+import { PacmanActions } from "~/store/pacmanSlice";
+import Ghost from "./ghost";
 
 export default function Game() {
   const game = useSelector((state: RootState) => state.game);
+  const pacman = useSelector((state: RootState) => state.pacman);
+  const blinky = useSelector((state: RootState) => state.blinky); 
+  const inky = useSelector((state: RootState) => state.inky);
+  const pinky = useSelector((state: RootState) => state.pinky);
+  const clyde = useSelector((state: RootState) => state.clyde);
+  const ghosts = [blinky, inky, pinky, clyde];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (game.gameStarted) {
+      dispatch(PacmanActions.startingState());
+    }
+  }, []);
   return (
     <Stage
       width={SIZES.MAP.WIDTH}
@@ -18,7 +34,17 @@ export default function Game() {
       }}
     >
       <Level level={game.level}/>
-      <Pacman x={CHAR_SPAWNS.PACMAN.x} y={CHAR_SPAWNS.PACMAN.y} state="1" direction={DIRECTIONS.LEFT}/>
+      <Pacman x={pacman.x} y={pacman.y} state={pacman.state} direction={pacman.direction}/>
+      {ghosts.map((ghost) => (
+        <Ghost
+          key={ghost.name}
+          x={ghost.x}
+          y={ghost.y}
+          state={ghost.state}
+          direction={ghost.direction}
+          name={ghost.name}
+        />
+      ))}
     </Stage>
   );
 }
