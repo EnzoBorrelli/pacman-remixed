7fedf65c354d1386fd2cc472f76fda78c1502fe4
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-20 09:36:24 -0300

    added ghosts slices

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  gameReducer  from "./gameSlice";
+import  pacmanReducer  from "./pacmanSlice";
+import { blinkyReducer, clydeReducer, inkyReducer, pinkyReducer } from "./ghostSlices";
 
 export const store = configureStore({
   reducer: {
     game: gameReducer,
+    pacman: pacmanReducer,
+    blinky:blinkyReducer,
+    inky:inkyReducer,
+    pinky:pinkyReducer,
+    clyde:clydeReducer
   },
 });
 
