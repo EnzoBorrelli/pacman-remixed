a4fe8e106830c0181b4150ddaa29936cb1d26a8c
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-23 14:17:17 -0300

    changed status logic

--- app/store/gameSlice.ts
+++ app/store/gameSlice.ts
@@ -1,48 +1,52 @@
-import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { GAME_STATUS } from "~/consts/game";
 import { iGameState } from "~/interfaces/slices";
 
-const initialState : iGameState = {
-    gameStarted: false,
-    gameOver: false,
-    score: 0,
-    highScore:0,
-    level: 0,
-    lives: 3,  
-    fruits: [],  
+const initialState: iGameState = {
+  status:GAME_STATUS.STARTED,
+  score: 0,
+  highScore: 0,
+  level: 0,
+  lives: 3,
+  fruits: [],
 };
 
 const gameSlice = createSlice({
-    name: "game",
-    initialState,
-    reducers: {
-        startGame: (state) => {
-            state.gameStarted = true;
-            state.gameOver = false;
-        },
-        endGame: (state) => {
-            state.gameStarted = false;
-            state.gameOver = true;
-            state.score = 0;
-            state.level = 0;
-            state.lives = 3;
-            state.fruits = [];
-        },
-        increaseScore: (state, action: PayloadAction<number>) => {
-            state.score += action.payload;
-        },
-        setHighScore: (state, action: PayloadAction<number>) => {
-            state.score += action.payload;
-        },
-        nextLevel: (state) => {
-            state.level += 1;
-        },
-        loseLife: (state) => {
-            state.lives -= 1;
-            if (state.lives <= 0) {
-                state.gameOver = true;
-            }
-        },
+  name: "game",
+  initialState,
+  reducers: {
+    startGame: (state) => {
+      state.status = GAME_STATUS.STARTED;
     },
+    endGame: (state) => {
+      state.status = GAME_STATUS.OVER;
+      if (state.score > state.highScore) {
+        state.highScore = state.score;
+      }
+      state.score = 0;
+      state.level = 0;
+      state.lives = 3;
+      state.fruits = [];
+    },
+    increaseScore: (state, action: PayloadAction<number>) => {
+      state.score += action.payload;
+    },
+    setHighScore: (state, action: PayloadAction<number>) => {
+      state.score += action.payload;
+    },
+    nextLevel: (state) => {
+      state.level += 1;
+    },
+    loseLife: (state) => {
+      state.lives -= 1;
+      if (state.lives <= 0) {
+        state.status = GAME_STATUS.OVER;
+      }
+    },
+    setFruits: (state, action: PayloadAction<number[]>) => {
+      state.fruits = action.payload;
+    }
+  },
 });
 
 export const GameActions = gameSlice.actions;
