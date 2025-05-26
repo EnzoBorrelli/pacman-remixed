0199c03d0427714c1b133c4855ed2b510608819a
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-26 11:42:30 -0300

    moved pelletarray to coord to pelletUtils

--- app/components/level.tsx
+++ app/components/level.tsx
@@ -1,31 +1,16 @@
 import { Container, Sprite } from "@pixi/react";
 import map from "/assets/spritesheets/map.png";
 import { iPellet } from "~/interfaces/components";
-import { useEffect, useState } from "react";
-import { PELLETS_MAP } from "~/consts/map";
 import Pellet from "./pellet";
-import { CHAR_SPAWNS, SIZES } from "~/consts/game";
+import { CHAR_SPAWNS } from "~/consts/game";
 import Fruit from "./fruit";
 
-export default function Level({level}: { level: number }) {
-  const [pellets, setPellets] = useState<iPellet[]>([]);
-
+export default function Level({level,pellets}: { level: number,pellets: iPellet[] }) {
+  
   const fruitType = level > 7 ? 7 : level;
   
-
-  useEffect(() => {
-    const pelletsArray = [];
-    for (let row = 0; row < PELLETS_MAP.length; row++) {
-      for (let col = 0; col < PELLETS_MAP[row].length; col++) {
-        if (PELLETS_MAP[row][col] !== 0) {
-          const x = col * SIZES.TILE;
-          const y = row * SIZES.TILE;
-          pelletsArray.push({ x, y, type: PELLETS_MAP[row][col] });
-        }
-      }
-    }
-    setPellets(pelletsArray);
-  }, []);
+  
+ 
   return (
     <Container x={0} y={0}>
       <Sprite image={map} />
