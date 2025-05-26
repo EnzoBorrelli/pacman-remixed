8e49884f6bc8caad9c9fc0a66a5f67c8ed4be072
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-26 11:41:23 -0300

    Create pelletUtils.ts

---
+++ app/scripts/objects/pelletUtils.ts
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { PACMAN_STATES, SIZES } from "~/consts/game";
+import { PELLETS_MAP } from "~/consts/map";
+import { GameActions } from "~/store/gameSlice";
+import { PacmanActions } from "~/store/pacmanSlice";
+import { useGameloop } from "../useGameLoop";
+import { iPellet } from "~/interfaces/components";
+import { isCollidingWithObject } from "~/utils/isColliding";
+import soundPlayer from "~/utils/soundPlayer";
+
+function useMapPellets(pelletsMap: number[][] = PELLETS_MAP) {
+  const [pellets, setPellets] = useState<iPellet[]>([]);
+
+  useEffect(() => {
+    const pelletsArray: iPellet[] = [];
+    for (let row = 0; row < pelletsMap.length; row++) {
+      for (let col = 0; col < pelletsMap[row].length; col++) {
+        if (pelletsMap[row][col] !== 0) {
+          const x = col * SIZES.TILE;
+          const y = row * SIZES.TILE;
+          pelletsArray.push({
+            x,
+            y,
+            type: pelletsMap[row][col],
+            isEaten: false,
+          });
+        }
+      }
+    }
+    setPellets(pelletsArray);
+  }, [pelletsMap]);
+
+  return pellets;
+}
+
+export function usePelletCollision(
+  pacmanX: number,
+  pacmanY: number
+) {
+  const dispatch = useDispatch();
+  const initialPellets = useMapPellets();
+  const [visiblePellets, setVisiblePellets] = useState<iPellet[]>([]);
+  const pelletsRef = useRef<iPellet[]>([]);
+
+  // Set once pellets are loaded
+  useEffect(() => {
+    pelletsRef.current = initialPellets;
+    setVisiblePellets(initialPellets);
+  }, [initialPellets]);
+
+  useGameloop(() => {
+    let changed = false;
+    for (const pellet of pelletsRef.current) {
+      if (pellet.isEaten) continue;
+      const isCollided = isCollidingWithObject(
+        pacmanX,
+        pacmanY,
+        SIZES.TILE,
+        pellet.x,
+        pellet.y,
+      );
+      if (isCollided) {
+        console.log("Collision detected with pellet at", pellet.x, pellet.y);
+        dispatch(GameActions.removePellet({ x: pellet.x, y: pellet.y }));
+        dispatch(PacmanActions.setEatenPellets());
+
+        if (pellet.type === 2) {
+          dispatch(PacmanActions.setState(PACMAN_STATES.EATING_POWER_PELLET));
+          soundPlayer.PlaySound({folder:"gameplay",audio:"eat_dot_1"})
+          dispatch(GameActions.increaseScore(50));
+        } else {
+          dispatch(GameActions.increaseScore(10));
+          soundPlayer.PlaySound({folder:"gameplay",audio:"eat_dot_0"})
+        }
+
+        pellet.isEaten = true;
+        changed = true;
+      }
+      if(changed){
+        setVisiblePellets([...pelletsRef.current.filter((p) => !p.isEaten)]);
+      }
+    }
+  });
+
+  return visiblePellets
+}
