e65ed3e3f68553c0b4ad8aab334a7b4d48d6bca5
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-26 11:42:03 -0300

    added a dynamic anchor

--- app/components/pacman.tsx
+++ app/components/pacman.tsx
@@ -7,6 +7,7 @@ import { useAnimations } from "~/animations/pacmanAnimations";
 
 export default function Pacman({ x, y, state, direction }: iPacman) {
   const animation = useAnimations({ state, direction });
+  const anchorX = state === PACMAN_STATES.MOVING ? 0.4 : 0;
 
   if (!animation || animation.textures.length === 0) return null;
 
@@ -19,7 +20,7 @@ export default function Pacman({ x, y, state, direction }: iPacman) {
       isPlaying={state !== PACMAN_STATES.IDLE}
       animationSpeed={state === PACMAN_STATES.EATING_POWER_PELLET ? 0.2 : 0.1}
       loop={state !== PACMAN_STATES.DYING}
-      anchor={{ x: 0.4, y: 0.3 }}
+      anchor={{ x: anchorX, y: 0.3 }}
     />
   );
 }
