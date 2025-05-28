78ff2edd2e3220ebc09553d5e1d4034431402814
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-28 09:31:31 -0300

    fix render issue

--- app/ui/level_ui/fruitArray.tsx
+++ app/ui/level_ui/fruitArray.tsx
@@ -1,35 +1,44 @@
 import { Container, Sprite } from "@pixi/react";
-import { Assets, Rectangle, Texture } from "pixi.js";
+import { Assets, BaseTexture, Rectangle, Texture } from "pixi.js";
 import { useEffect, useState } from "react";
 import { SIZES } from "~/consts/game";
 
 export default function FruitArray({ fruitArray }: { fruitArray: number[] }) {
   const [textures, setTextures] = useState<Texture[]>([]);
+  const [baseTexture, setBaseTexture] = useState<BaseTexture | null>(null);
+
   useEffect(() => {
     const load = async () => {
-      const baseTexture = await Assets.load("/assets/spritesheets/fruit.png");
-      const frameTextures: Texture[] = [];
-
-      for (let i = 0; i < fruitArray.length; i++) {
-        frameTextures.push(
-          new Texture(
-            baseTexture,
-            new Rectangle(
-              fruitArray[i] * SIZES.CHARACTER,
-              0,
-              SIZES.CHARACTER,
-              SIZES.CHARACTER
-            )
-          )
-        );
-      }
-      setTextures(frameTextures);
+      const texture = await Assets.load("/assets/spritesheets/fruit.png");
+      setBaseTexture(texture);
     };
     load();
   }, []);
 
+  useEffect(() => {
+    if (!baseTexture) return;
+
+    const newTextures = fruitArray.map(
+      (fruitIndex) =>
+        new Texture(
+          baseTexture,
+          new Rectangle(
+            fruitIndex * SIZES.CHARACTER,
+            0,
+            SIZES.CHARACTER,
+            SIZES.CHARACTER
+          )
+        )
+    );
+
+    // Optionally dispose of old textures here to free GPU memory
+    textures.forEach((t) => t.destroy());
+
+    setTextures(newTextures);
+  }, [fruitArray, baseTexture]);
+
   return (
-    <Container x={SIZES.MAP.WIDTH-24} y={16}>
+    <Container x={SIZES.MAP.WIDTH - 24} y={16}>
       {textures.map((texture, index) => (
         <Sprite
           key={index}
