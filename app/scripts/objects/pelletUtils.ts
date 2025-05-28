dd2679635c340faef67619fd9a8369b619bcf46a
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-05-28 13:43:32 -0300

    Update pelletUtils.ts

--- app/scripts/objects/pelletUtils.ts
+++ app/scripts/objects/pelletUtils.ts
@@ -34,10 +34,7 @@ function useMapPellets(pelletsMap: number[][] = PELLETS_MAP) {
   return pellets;
 }
 
-export function usePelletCollision(
-  pacmanX: number,
-  pacmanY: number
-) {
+export function usePelletCollision(pacmanX: number, pacmanY: number) {
   const dispatch = useDispatch();
   const initialPellets = useMapPellets();
   const [visiblePellets, setVisiblePellets] = useState<iPellet[]>([]);
@@ -51,7 +48,7 @@ export function usePelletCollision(
 
   useGameloop(() => {
     let changed = false;
-    dispatch(GameActions.setHighScore())
+    dispatch(GameActions.setHighScore());
     for (const pellet of pelletsRef.current) {
       if (pellet.isEaten) continue;
       const isCollided = isCollidingWithObject(
@@ -59,7 +56,7 @@ export function usePelletCollision(
         pacmanY,
         SIZES.TILE,
         pellet.x,
-        pellet.y,
+        pellet.y
       );
       if (isCollided) {
         dispatch(GameActions.removePellet({ x: pellet.x, y: pellet.y }));
@@ -68,21 +65,29 @@ export function usePelletCollision(
         if (pellet.type === 2) {
           dispatch(PacmanActions.setState(PACMAN_STATES.EATING_POWER_PELLET));
           dispatch(PacmanActions.resetPowerPelletTimeout());
-          soundPlayer.PlaySound({folder:"gameplay",audio:"eat_dot_1"})
+          soundPlayer.PlaySound({
+            folder: "gameplay",
+            audio: "eat_dot_1",
+            useCache: true,
+          });
           dispatch(GameActions.increaseScore(50));
         } else {
           dispatch(GameActions.increaseScore(10));
-          soundPlayer.PlaySound({folder:"gameplay",audio:"eat_dot_0"})
+          soundPlayer.PlaySound({
+            folder: "gameplay",
+            audio: "eat_dot_0",
+            useCache: true,
+          });
         }
 
         pellet.isEaten = true;
         changed = true;
       }
-      if(changed){
+      if (changed) {
         setVisiblePellets([...pelletsRef.current.filter((p) => !p.isEaten)]);
       }
     }
   });
 
-  return visiblePellets
+  return visiblePellets;
 }
