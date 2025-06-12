b5ac937a1a7dc74ef80c92fe3ad46d17b0635c6e
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-12 11:38:58 -0300

    added target tile state

--- app/store/ghostSliceFactory.ts
+++ app/store/ghostSliceFactory.ts
@@ -1,44 +1,56 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { BEHAVIOR_STATES, GHOST_STATES } from "~/consts/game";
-import { iGhost } from "~/interfaces/slices"
+import { Direction, iGhost } from "~/interfaces/slices";
 
-
-
-const createGhostSlice = (name: string,spawn_x:number,spawn_y:number,spawn_direction:string) => {
-    const initialState: iGhost = {
-        x: spawn_x,
-        y: spawn_y,
-        state: GHOST_STATES.IDLE,
-        direction: spawn_direction,
-        name: name,
-        behavior: BEHAVIOR_STATES.SCATTER
-    };
-    return createSlice({
-        name: name,
-        initialState,
-        reducers: {
-            reset: (state) => {
-                state.x = spawn_x;
-                state.y = spawn_y;
-                state.state = GHOST_STATES.IDLE;
-                state.direction = spawn_direction;
-                state.behavior= BEHAVIOR_STATES.SCATTER
-            },
-            setGCoordinates: (state, action) => {
-                state.x = action.payload.x;
-                state.y = action.payload.y;
-            },
-            setState: (state, action:PayloadAction<string>) => {
-                state.state = action.payload;
-            },
-            setDirection: (state, action:PayloadAction<string>) => {
-                state.direction = action.payload;
-            },
-            setBehavior: (state, action:PayloadAction<string>) => {
-                state.behavior = action.payload;
-            },
-        },
-    });
+const createGhostSlice = (
+  name: string,
+  spawn_x: number,
+  spawn_y: number,
+  spawn_direction: Direction,
+  targetTile: { x: number; y: number }
+) => {
+  const initialState: iGhost = {
+    x: spawn_x,
+    y: spawn_y,
+    state: GHOST_STATES.IDLE,
+    direction: spawn_direction,
+    name: name,
+    behavior: BEHAVIOR_STATES.SCATTER,
+    targetTile: targetTile,
+  };
+  return createSlice({
+    name: name,
+    initialState,
+    reducers: {
+      reset: (state) => {
+        state.x = spawn_x;
+        state.y = spawn_y;
+        state.state = GHOST_STATES.IDLE;
+        state.direction = spawn_direction;
+        state.behavior = BEHAVIOR_STATES.SCATTER;
+        state.targetTile = targetTile;
+      },
+      setGCoordinates: (state, action) => {
+        state.x = action.payload.x;
+        state.y = action.payload.y;
+      },
+      setState: (state, action: PayloadAction<string>) => {
+        state.state = action.payload;
+      },
+      setDirection: (state, action: PayloadAction<Direction>) => {
+        state.direction = action.payload;
+      },
+      setBehavior: (state, action: PayloadAction<string>) => {
+        state.behavior = action.payload;
+      },
+      setTargetTile: (
+        state,
+        action: PayloadAction<{ x: number; y: number }>
+      ) => {
+        state.targetTile = action.payload;
+      },
+    },
+  });
 };
 
 export default createGhostSlice;
--- app/store/ghostSlices.ts
+++ app/store/ghostSlices.ts
@@ -1,11 +1,11 @@
-import { CHAR_SPAWNS, DIRECTIONS } from "~/consts/game";
+import { CHAR_SPAWNS, DIRECTIONS, SCATTER_TILES } from "~/consts/game";
 import createGhostSlice from "./ghostSliceFactory";
 
 //slices
-const blinkySlice = createGhostSlice("blinky", CHAR_SPAWNS.BLINKY.x, CHAR_SPAWNS.BLINKY.y,DIRECTIONS.LEFT);
-const pinkySlice = createGhostSlice("pinky", CHAR_SPAWNS.PINKY.x, CHAR_SPAWNS.PINKY.y,DIRECTIONS.DOWN);
-const inkySlice = createGhostSlice("inky", CHAR_SPAWNS.INKY.x, CHAR_SPAWNS.INKY.y,DIRECTIONS.UP);
-const clydeSlice = createGhostSlice("clyde", CHAR_SPAWNS.CLYDE.x, CHAR_SPAWNS.CLYDE.y,DIRECTIONS.UP);
+const blinkySlice = createGhostSlice("blinky", CHAR_SPAWNS.BLINKY.x, CHAR_SPAWNS.BLINKY.y,DIRECTIONS.LEFT,SCATTER_TILES.BLINKY);
+const pinkySlice = createGhostSlice("pinky", CHAR_SPAWNS.PINKY.x, CHAR_SPAWNS.PINKY.y,DIRECTIONS.DOWN,SCATTER_TILES.PINKY);
+const inkySlice = createGhostSlice("inky", CHAR_SPAWNS.INKY.x, CHAR_SPAWNS.INKY.y,DIRECTIONS.UP,SCATTER_TILES.INKY);
+const clydeSlice = createGhostSlice("clyde", CHAR_SPAWNS.CLYDE.x, CHAR_SPAWNS.CLYDE.y,DIRECTIONS.UP,SCATTER_TILES.CLYDE);
 
 //actions
 export const BlinkyActions = blinkySlice.actions;
