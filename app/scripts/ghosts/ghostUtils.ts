8d4c23fcd7d50efd562bfeab2902c2c4e98bdb73
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-17 11:52:48 -0300

    replace move to target with set target action

--- app/scripts/ghosts/ghostUtils.ts
+++ app/scripts/ghosts/ghostUtils.ts
@@ -2,9 +2,11 @@ import { Dispatch } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import {
   BEHAVIOR_STATES,
+  CAGE_TILES,
   GAME_STATUS,
   GHOST_STATES,
   PACMAN_STATES,
+  SCATTER_TILES,
   SIZES,
 } from "~/consts/game";
 import { ghostActions } from "~/interfaces/components";
@@ -12,7 +14,7 @@ import { iGhost, iPacman } from "~/interfaces/slices";
 import { RootState } from "~/store";
 import { GameActions } from "~/store/gameSlice";
 import { isCollidingWithObject } from "~/utils/isColliding";
-import { moveToTarget } from "./moveToTarget";
+import { getRandomTile } from "./travelUtils";
 
 type iState = {
   ghost: iGhost;
@@ -22,18 +24,29 @@ type iState = {
 
 function chaseState() {}
 
-function scatterState(
-  { ghost, actions, dispatch }: iState,
-  pacmanX: number,
-  pacmanY: number
-) {
-  moveToTarget(ghost, actions, pacmanX, pacmanY, dispatch);
+function cageState({ ghost, actions, dispatch }: iState) {
+  dispatch(actions.setTargetTile(CAGE_TILES[ghost.name!]));
+  dispatch(actions.setState(GHOST_STATES.MOVING));
+  console.log("im in cage state");
+  if (
+    ghost.x / SIZES.TILE === ghost.targetTile?.x &&
+    ghost.y / SIZES.TILE === ghost.targetTile?.y
+  ) {
+    dispatch(actions.setBehavior(BEHAVIOR_STATES.SCATTER));
+  }
+}
+function scatterState({ ghost, actions, dispatch }: iState) {
+  dispatch(actions.setState(GHOST_STATES.MOVING));
+  dispatch(actions.setTargetTile(SCATTER_TILES[ghost.name!]));
+  console.log("im in scatter state");
 }
 
 function frightenedState(
   { ghost, actions, dispatch }: iState,
   powerTime: number
 ) {
+  const randomTile = getRandomTile();
+  dispatch(actions.setTargetTile(randomTile));
   if (powerTime > 400) {
     dispatch(actions.setState(GHOST_STATES.FRIGHTENED));
   } else if (powerTime < 400 && powerTime > 0) {
@@ -43,21 +56,27 @@ function frightenedState(
 
 function eatenState({ ghost, actions, dispatch }: iState) {
   dispatch(actions.setState(GHOST_STATES.DEAD));
+  dispatch(actions.setTargetTile(CAGE_TILES[ghost.name!]));
+  if (
+    ghost.x / SIZES.TILE === ghost.targetTile?.x &&
+    ghost.y / SIZES.TILE === ghost.targetTile?.y
+  ) {
+    dispatch(actions.setBehavior(BEHAVIOR_STATES.CAGE));
+  }
 }
 
 export function useBehaviorManager() {
   const pacman = useSelector((state: RootState) => state.pacman);
   const dispatch = useDispatch();
   return (ghost: iGhost, actions: ghostActions) => {
+    if (ghost.behavior === BEHAVIOR_STATES.CAGE) {
+      cageState({ ghost: ghost, actions: actions, dispatch: dispatch });
+    }
     if (ghost.behavior === BEHAVIOR_STATES.CHASE) {
       chaseState();
     }
     if (ghost.behavior === BEHAVIOR_STATES.SCATTER) {
-      scatterState(
-        { ghost: ghost, actions: actions, dispatch: dispatch },
-        pacman.x,
-        pacman.y
-      );
+      scatterState({ ghost: ghost, actions: actions, dispatch: dispatch });
     }
     if (ghost.behavior === BEHAVIOR_STATES.FRIGHTENED) {
       if (pacman.powerPelletTimeout != null)
