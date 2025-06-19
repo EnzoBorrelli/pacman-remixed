0c42488c17a699f0e97bd6d00abd1a8b06943031
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-19 14:04:23 -0300

    Update components.ts

--- app/interfaces/components.ts
+++ app/interfaces/components.ts
@@ -1,24 +1,33 @@
 import { BlinkyActions } from "~/store/ghostSlices";
+import { iGameState, iGhost, iPacman } from "./slices";
+import { Dispatch } from "@reduxjs/toolkit";
 
-export interface iPellet{
-    x: number;
-    y: number;
-    type: number;
-    isEaten?: boolean;
+export interface iPellet {
+  x: number;
+  y: number;
+  type: number;
+  isEaten?: boolean;
 }
 
-export interface iFruit{
-    x: number;
-    y: number;
-    type: number;
-    isEaten?: boolean;
+export interface iFruit {
+  x: number;
+  y: number;
+  type: number;
+  isEaten?: boolean;
 }
 
-export interface iEatScoreDisplay{
-    x: number;
-    y: number;
-    isVisible: boolean;
-    score: number;
+export interface iEatScoreDisplay {
+  x: number;
+  y: number;
+  isVisible: boolean;
+  score: number;
 }
 
-export type ghostActions = typeof BlinkyActions
+export type ghostActions = typeof BlinkyActions;
+
+export interface gameFuntion {
+  pacman: iPacman;
+  ghosts: iGhost[];
+  game: iGameState;
+  dispatch: Dispatch;
+}
