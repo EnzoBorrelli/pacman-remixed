0b8daff6c4158ae5402c293ce20d86f142ef0dae
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-23 15:41:37 -0300

    provisional credit

--- app/routes/credits.tsx
+++ app/routes/credits.tsx
@@ -1,7 +1,10 @@
+import UITop from "~/ui/level_ui/ui_top";
+
 export default function Credits() {
   return (
     <main className="relative flex flex-col items-center justify-center h-screen text-white bg-slate-950">
-      hi
+      <UITop/>
+      <h2>not finished</h2>
     </main>
   );
 }
