445e20aa86191874463928fc1ffaa4f49e93a00f
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-06-24 11:04:07 -0300

    added version to logo

--- app/ui/menu_ui/logo.tsx
+++ app/ui/menu_ui/logo.tsx
@@ -1,27 +1,26 @@
-
-
 export default function Logo() {
   return (
-      <div className="relative w-[580px] bg-red-600 rounded-2xl h-[9rem]">
-        <div className="absolute w-[96%] top-3 left-3 h-[7.5rem] bg-orange-500 rounded-lg ring-4 ring-slate-950"></div>
-        <h2 className="absolute text-3xl font-bold tracking-widest text-center text-purple-700 text-stroke-1 top-11 left-[4rem]">
-          PACMAN REMIXED
-        </h2>
-        <h2 className="absolute text-3xl font-bold tracking-widest text-center text-slate-100 text-stroke-2 top-9 left-[3.5rem]">
-          PACMAN REMIXED
-        </h2>
-        <h2 className="absolute text-3xl font-bold tracking-widest text-center text-yellow-400 text-stroke-3 top-10 left-[3.75rem]">
-          PACMAN REMIXED
-        </h2>
-        <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-950 text-stroke-3 top-[5.75rem] left-[9.25rem]">
-          by EndyKaishi®
-        </h3>
-        <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-600 text-stroke-2 top-[5.25rem] left-[8.75rem]">
-          by EndyKaishi®
-        </h3>
-        <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-200 text-stroke-1 top-[5.5rem] left-36">
-          by EndyKaishi®
-        </h3>
-      </div>
+    <div className="relative w-[580px] bg-red-600 rounded-2xl h-[9rem]">
+      <div className="absolute w-[96%] top-3 left-3 h-[7.5rem] bg-orange-500 rounded-lg ring-4 ring-slate-950"></div>
+      <h2 className="absolute text-3xl font-bold tracking-widest text-center text-purple-700 text-stroke-1 top-11 left-[4rem]">
+        PACMAN REMIXED
+      </h2>
+      <h2 className="absolute text-3xl font-bold tracking-widest text-center text-slate-100 text-stroke-2 top-9 left-[3.5rem]">
+        PACMAN REMIXED
+      </h2>
+      <h2 className="absolute text-3xl font-bold tracking-widest text-center text-yellow-400 text-stroke-3 top-10 left-[3.75rem]">
+        PACMAN REMIXED
+      </h2>
+      <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-950 text-stroke-3 top-[5.75rem] left-[9.25rem]">
+        by EndyKaishi®
+      </h3>
+      <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-600 text-stroke-2 top-[5.25rem] left-[8.75rem]">
+        by EndyKaishi®
+      </h3>
+      <h3 className="absolute text-xl font-bold tracking-widest text-center text-teal-200 text-stroke-1 top-[5.5rem] left-36">
+        by EndyKaishi®
+      </h3>
+      <h4 className="absolute font-sans font-bold text-xs text-white top-[7rem] right-6"> v 1.0.0</h4>
+    </div>
   );
 }
